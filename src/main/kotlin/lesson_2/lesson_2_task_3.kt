2bd9b5cc5d7275d8079390f553b5e222bd9fd690
Softheitron <softheitron@gmail.com>
2024-09-21 20:23:48 +0500

    KS-2-3 task approve request(вообще не уверен что это првильно, но работает :) )

---
+++ src/main/kotlin/lesson_2/lesson_2_task_3.kt
@@ -0,0 +1,20 @@
+package org.example.lesson_2
+
+fun main() {
+
+    val departureHour = 9
+    val departureMinutes = 47
+    val timeToArriveInMinutes = 457
+
+    var arriveHour = departureHour + timeToArriveInMinutes/60
+    val restMinutes = timeToArriveInMinutes - (timeToArriveInMinutes/60 * 60)
+    var arriveMinute = departureMinutes + restMinutes
+
+    if (arriveMinute > 60) {
+        arriveHour++
+        arriveMinute -= 60
+    }
+
+    print("Train will arrive at $arriveHour:$arriveMinute")
+
+}
