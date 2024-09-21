82128354475de1265056f4c8e580e614ed1a21d2
Softheitron <softheitron@gmail.com>
2024-09-21 19:37:01 +0500

    KS-1-5 fifth task approve request

---
+++ src/main/kotlin/lesson_2/lesson_2_task_1.kt
@@ -0,0 +1,14 @@
+package org.example.lesson_2
+
+fun main() {
+
+    val studentA = 3f
+    val studentB = 4f
+    val studentC = 3f
+    val studentD = 5f
+
+    val mean = (studentA+studentB+studentC+studentD)/4
+
+    print(mean)
+
+}
