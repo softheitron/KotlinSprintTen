8d78d6baa04cd1872d64edb8e484e7ee893bde6b
Softheitron <softheitron@gmail.com>
2024-09-21 15:15:41 +0500

    KS-1-2 second task approve request

---
+++ src/main/kotlin/lesson_1/lesson_1_task_2.kt
@@ -0,0 +1,20 @@
+package org.example.lesson_1
+
+/*
+
+ */
+fun main() {
+
+    val ordersAmount = 75
+    val thankfulMessage = "Thank you for your purchase!"
+
+    println("$thankfulMessage \n$ordersAmount total!")
+
+    var numberOfEmployees = 2000
+
+//    println(numberOfEmployees)
+
+    numberOfEmployees -= 1
+
+    println("Employees number after dismission - $numberOfEmployees")
+}
