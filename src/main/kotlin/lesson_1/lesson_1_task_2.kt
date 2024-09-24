4f02843ba0e1b69bd9f7dd0a60611241f47a5594
Softheitron <softheitron@gmail.com>
2024-09-24 16:56:29 +0500

    KS-1-2 removed comments

--- src/main/kotlin/lesson_1/lesson_1_task_2.kt
+++ src/main/kotlin/lesson_1/lesson_1_task_2.kt
@@ -1,8 +1,5 @@
 package org.example.lesson_1
 
-/*
-
- */
 fun main() {
 
     val ordersAmount = 75
@@ -12,9 +9,8 @@ fun main() {
 
     var numberOfEmployees = 2000
 
-//    println(numberOfEmployees)
-
     numberOfEmployees -= 1
 
     println("Employees number after dismission - $numberOfEmployees")
+
 }
