d64c0d300b89a23d46711db195f44b847b68e999
Softheitron <softheitron@gmail.com>
2024-09-25 15:48:23 +0500

    KS-5-5 when-list-random

---
+++ src/main/kotlin/lesson_5/lesson_5_task_5.kt
@@ -0,0 +1,42 @@
+package org.example.lesson_5
+
+import kotlin.random.Random
+
+fun main() {
+
+    println(
+        "Добро пожаловать на двойное колесо удачи!\n" +
+                "Правила игры:\n" +
+                "Что бы выиграть, вам нужно угадать три числа в диапазоне от 0 до 42\n" +
+                "Если отгадать два из трех загаданных чисел, вы получите крупный приз!\n" +
+                "Если отгадать одно из трех чисел, вы получите утешительный приз!"
+    )
+
+    val firstNumber = Random.nextInt(0, 43)
+    val secondNumber = Random.nextInt(0, 43)
+    val thirdNumber = Random.nextInt(0, 43)
+    val winNumList = listOf(firstNumber, secondNumber, thirdNumber)
+    println("Числа были сгенерированы!\nУдачи!")
+
+    print("Введите первое число: ")
+    val firstNumInput = readln().toInt()
+    print("Введите второе число: ")
+    val secondNumInput = readln().toInt()
+    print("Введите третье число: ")
+    val thirdNumInput = readln().toInt()
+    val userNumList = listOf(firstNumInput, secondNumInput, thirdNumInput)
+
+    val numberOfMatches = winNumList.intersect(userNumList).size
+
+    println(
+        "Выбранные программой числа: $firstNumber, $secondNumber, $thirdNumber\n" +
+        "Выбранные вами числа: $firstNumInput, $secondNumInput, $thirdNumInput"
+    )
+
+    when(numberOfMatches) {
+        1 -> print("Вы выиграли утешительный приз!")
+        2 -> print("Вы выиграли крупный приз!")
+        3 -> print("Невероятное везение! Вы выиграли ДЖЕКПОТ!")
+    }
+
+}
