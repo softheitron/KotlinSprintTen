639fcc467d3cd5f29821f2b6faec96139cde309c
Softheitron <softheitron@gmail.com>
2024-09-25 20:55:21 +0500

    KS-2-1 minor-changes

--- src/main/kotlin/lesson_2/lesson_2_task_1.kt
+++ src/main/kotlin/lesson_2/lesson_2_task_1.kt
@@ -6,8 +6,9 @@ fun main() {
     val studentB = 4f
     val studentC = 3f
     val studentD = 5f
+    val studentsAmount = 4
 
-    val mean = (studentA + studentB + studentC + studentD)/4
+    val mean = (studentA + studentB + studentC + studentD) / studentsAmount
 
     print(mean)
 
