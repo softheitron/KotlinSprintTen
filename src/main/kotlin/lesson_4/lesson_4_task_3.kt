874a749a6151805733662549c516a15770adda8e
Softheitron <softheitron@gmail.com>
2024-09-26 23:17:10 +0500

    KS-4-3 added-constants-technical-fix

--- src/main/kotlin/lesson_4/lesson_4_task_3.kt
+++ src/main/kotlin/lesson_4/lesson_4_task_3.kt
@@ -1,14 +1,19 @@
 package org.example.lesson_4
 
+const val REQUIRED_SUNNY_WEATHER = true
+const val REQUIRED_AWNING_POSITION = true
+const val REQUIRED_AIR_HUMIDITY = 20
+const val WRONG_TIME_OF_YEAR = "зима"
+
 fun main() {
 
-    val isWeatherSunny: Boolean = true
-    val isAwningOpen: Boolean = true
-    val airHumidity: Int = 20
-    val timeOfYear: String = "зима"
+    val sunnyWeather = true
+    val awningOpen = true
+    val airHumidity = 20
+    val todayTimeOfYear = "зима"
 
     val isGrowPossible: Boolean =
-        isWeatherSunny == true && isAwningOpen == true && airHumidity == 20 && timeOfYear != "зима"
+        todayTimeOfYear != WRONG_TIME_OF_YEAR && sunnyWeather == REQUIRED_SUNNY_WEATHER && airHumidity == REQUIRED_AIR_HUMIDITY && awningOpen == REQUIRED_AWNING_POSITION
 
     println("Благоприятные ли условия для роста бобовых?\n$isGrowPossible")
 
