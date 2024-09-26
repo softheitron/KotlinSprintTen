535517d7a6ba8fd11e2d37f4ed1c2a597286ed86
Softheitron <softheitron@gmail.com>
2024-09-26 20:27:20 +0500

    KS-2-4 added-percentage-const

--- src/main/kotlin/lesson_2/lesson_2_task_4.kt
+++ src/main/kotlin/lesson_2/lesson_2_task_4.kt
@@ -1,13 +1,15 @@
 package org.example.lesson_2
 
+const val PERCENTAGE_RATE = 100f
+
 fun main() {
 
-    val crystalOreMined = 7
+    val crystalOreMined = 10
     val ironOreMined = 11
-    val buff = 0.20f
+    val buff = 20
 
-    val crystalOreWithBuff = crystalOreMined * buff
-    val ironOreWithBuff = ironOreMined * buff
+    val crystalOreWithBuff = crystalOreMined * (buff / PERCENTAGE_RATE)
+    val ironOreWithBuff = ironOreMined * (buff / PERCENTAGE_RATE)
 
     println(crystalOreWithBuff.toInt())
     println(ironOreWithBuff.toInt())
