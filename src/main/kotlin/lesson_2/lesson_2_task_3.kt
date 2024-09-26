3a946762705c2fbc3297f9dc8551f56246a8f76e
Softheitron <softheitron@gmail.com>
2024-09-26 22:09:08 +0500

    KS-2-3 minor-rework

--- src/main/kotlin/lesson_2/lesson_2_task_3.kt
+++ src/main/kotlin/lesson_2/lesson_2_task_3.kt
@@ -1,20 +1,20 @@
 package org.example.lesson_2
 
+const val SECONDS_IN_MINUTE = 60
+const val HOURS_IN_DAY = 24
+
 fun main() {
 
     val departureHour = 9
     val departureMinutes = 47
-    val timeToArriveInMinutes = 457
+    val timeToArriveInMinutes = 1290
 
-    var arriveHour = departureHour + timeToArriveInMinutes / 60
-    val restMinutes = timeToArriveInMinutes - (timeToArriveInMinutes / 60 * 60)
-    var arriveMinute = departureMinutes + restMinutes
+    val restMinutes = timeToArriveInMinutes % SECONDS_IN_MINUTE
+    val hoursToArrive = (timeToArriveInMinutes / SECONDS_IN_MINUTE) + ((departureMinutes + restMinutes) / SECONDS_IN_MINUTE)
+    val arriveMinute = (departureMinutes + restMinutes) % SECONDS_IN_MINUTE
+    val arriveHour = (departureHour + hoursToArrive) % HOURS_IN_DAY
 
-    if (arriveMinute > 60) {
-        arriveHour++
-        arriveMinute -= 60
-    }
-
-    print("Train will arrive at $arriveHour:$arriveMinute")
+    print("Train will arrive at %02d:%02d"
+        .format(arriveHour, arriveMinute))
 
 }
