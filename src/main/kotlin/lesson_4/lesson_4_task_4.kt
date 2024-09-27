f46df40992df8a4c253ffe0c2a2358138d9f4b89
Softheitron <softheitron@gmail.com>
2024-09-27 16:15:43 +0500

    KS-4-4 interpolation-rework

--- src/main/kotlin/lesson_4/lesson_4_task_4.kt
+++ src/main/kotlin/lesson_4/lesson_4_task_4.kt
@@ -4,21 +4,19 @@ const val CORE_STRING = "Упражнение для персса: "
 const val ARM_STRING = "Упражнение для рук: "
 const val BACK_STRING = "Упражнение для спины: "
 const val LEG_STRING = "Упражнение для ног: "
-const val LONGEST_MESSAGE = CORE_STRING.length
 
 fun main() {
 
-    val trainingDay = 5
-    val coreAndArmDay = (trainingDay == 1 || trainingDay in 5..100 step 4)
-    val legAndBackDay = (trainingDay == 3 || trainingDay in 7 .. 100 step 4)
-
-
+    val trainingDay = 6
+    val isCoreAndArmDay = trainingDay % 2 == 0
+    val isLegAndBackDay = !isCoreAndArmDay
 
     println(
-        ARM_STRING + " ".repeat(LONGEST_MESSAGE - ARM_STRING.length) + "$coreAndArmDay\n" +
-        LEG_STRING + " ".repeat(LONGEST_MESSAGE - LEG_STRING.length) + "$legAndBackDay\n" +
-        BACK_STRING + " ".repeat(LONGEST_MESSAGE - BACK_STRING.length) + "$legAndBackDay\n" +
-        CORE_STRING + " ".repeat(LONGEST_MESSAGE - CORE_STRING.length) + "$coreAndArmDay"
-    )
+        "%-25s %b\n%-25s %b\n%-25s %b\n%-25s %b".format(
+            ARM_STRING, isCoreAndArmDay,
+            LEG_STRING, isLegAndBackDay,
+            BACK_STRING, isLegAndBackDay,
+            CORE_STRING, isCoreAndArmDay
+        ))
 
 }
