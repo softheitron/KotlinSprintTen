2db117f0124df716da3e054322628d8f8aeba42c
Softheitron <softheitron@gmail.com>
2024-09-28 10:15:14 +0500

    KS-2-5 percentage-fix

--- src/main/kotlin/lesson_2/lesson_2_task_5.kt
+++ src/main/kotlin/lesson_2/lesson_2_task_5.kt
@@ -2,14 +2,16 @@ package org.example.lesson_2
 
 import kotlin.math.pow
 
+const val PERCENTAGE_RATE = 100
+
 fun main() {
 
     val startInvest = 70000
-    val interestRate = 0.167
+    val interestRate = 16.7
     val years = 20
+    val compoundInterestRate = (1 + (interestRate / PERCENTAGE_RATE)).pow(years)
 
-
-    val total = startInvest * (1 + interestRate).pow(years)
+    val total = startInvest * compoundInterestRate
 
     println(String.format("%.3f", total))
 
