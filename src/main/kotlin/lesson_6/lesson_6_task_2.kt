274057493da123f833be63c48a2683ba317109b1
Softheitron <softheitron@gmail.com>
2024-10-05 17:49:06 +0500

    KS-6-2 refactor

--- src/main/kotlin/lesson_6/lesson_6_task_2.kt
+++ src/main/kotlin/lesson_6/lesson_6_task_2.kt
@@ -1,15 +1,15 @@
 package org.example.lesson_6
 
+const val SECONDS_IN_MILLIS = 1000L
+
 fun main() {
 
     print("Введите число для отсчета: ")
     val countdownInput = readln().toInt()
-    var counter = countdownInput
+    val sleepTime = SECONDS_IN_MILLIS * countdownInput
 
-    while (counter > 0) {
-        Thread.sleep(1000)
-        counter--
-    }
+    Thread.sleep(sleepTime)
+
     println("Прошло $countdownInput секунд")
 
 }
