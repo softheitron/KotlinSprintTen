4203c7b6d7e6cd54b8cc3a39230c672d9f1be466
Softheitron <softheitron@gmail.com>
2024-10-07 01:58:47 +0500

    KS-8-3 minor fix

--- src/main/kotlin/lesson_8/lesson_8_task_3.kt
+++ src/main/kotlin/lesson_8/lesson_8_task_3.kt
@@ -7,9 +7,7 @@ fun main() {
     print("Введите название ингредиента для поиска в рецепте: ")
     val ingredientInput = readln()
 
-    val isRecipeContains = recipeIngredients.contains(ingredientInput.lowercase())
-
-    if (isRecipeContains) {
+    if (ingredientInput in recipeIngredients) {
         println("Ингредиент ${ingredientInput.lowercase()} в рецепте есть")
     } else {
         println("Такого ингредиента в рецепте нет")
