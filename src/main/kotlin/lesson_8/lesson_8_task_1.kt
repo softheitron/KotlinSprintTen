312f18fdeff156a53ef9253233adcb73645f21d3
Softheitron <softheitron@gmail.com>
2024-10-07 01:55:26 +0500

    KS-8-1 refactor

--- src/main/kotlin/lesson_8/lesson_8_task_1.kt
+++ src/main/kotlin/lesson_8/lesson_8_task_1.kt
@@ -2,15 +2,15 @@ package org.example.lesson_8
 
 fun main() {
 
-    val mondayStatistic = "23800"
-    val tuesdayStatistic = "32000"
-    val wednesdayStatistic = "18900"
-    val thursdayStatistic = "23800"
-    val fridayStatistic = "68798"
-    val saturdayStatistic = "158000"
-    val sundayStatistic = "232800"
+    val mondayStatistic = 23800
+    val tuesdayStatistic = 32000
+    val wednesdayStatistic = 18900
+    val thursdayStatistic = 23800
+    val fridayStatistic = 68798
+    val saturdayStatistic = 158000
+    val sundayStatistic = 232800
 
-    val weekStatisticsArray = arrayOf(
+    val weekStatisticsArray = intArrayOf(
         mondayStatistic,
         tuesdayStatistic,
         wednesdayStatistic,
@@ -19,11 +19,7 @@ fun main() {
         saturdayStatistic,
         sundayStatistic
     )
-    var weekStatisticSum = 0
-
-    for (i in weekStatisticsArray) {
-        weekStatisticSum += i.toInt()
-    }
+    val weekStatisticSum = weekStatisticsArray.sum()
 
     print("Количество просмотров за неделю составило: $weekStatisticSum")
 
