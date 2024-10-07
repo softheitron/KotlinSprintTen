0d21da0c3a868204cde356edcec82e91bd19d2e3
Softheitron <softheitron@gmail.com>
2024-10-07 16:45:25 +0500

    KS-7-2 while-refactor

--- src/main/kotlin/lesson_7/lesson_7_task_2.kt
+++ src/main/kotlin/lesson_7/lesson_7_task_2.kt
@@ -2,15 +2,20 @@ package org.example.lesson_7
 
 fun main() {
 
-    do {
-        val smsCode = (1000..9999).random()
+    var smsCode = (1000..9999).random()
+    println("Ваш код активации: $smsCode")
+
+    print("Введите код из sms: ")
+    var codeInput = readln().toInt()
+
+    while (smsCode != codeInput) {
+        println("Неверный код, попробуйте еще раз")
+        smsCode = (1000..9999).random()
         println("Ваш код активации: $smsCode")
         print("Введите код из sms: ")
-        val codeInput = readln().toInt()
-        if (codeInput == smsCode) {
-            print("Вход выполнен!\nДобро пожаловать!")
-            return
-        }
-    } while (true)
+        codeInput = readln().toInt()
+    }
+
+    print("Вход выполнен!\nДобро пожаловать!")
 
 }
