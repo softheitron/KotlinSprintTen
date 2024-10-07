92544a9312a6a092891ac2aee948b6bdd29e6fc5
Softheitron <softheitron@gmail.com>
2024-10-08 00:40:26 +0500

    KS-7-3 minor-fix

--- src/main/kotlin/lesson_7/lesson_7_task_3.kt
+++ src/main/kotlin/lesson_7/lesson_7_task_3.kt
@@ -4,9 +4,8 @@ fun main() {
 
     print("Введите число: ")
     val numberInput = readln().toInt()
-    val lastProgressionNumber = numberInput
 
-    for(i in 0 .. lastProgressionNumber step 2) {
+    for(i in 0 .. numberInput step 2) {
         println(i)
     }
 
