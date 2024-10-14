db8eb5d838a8f632cca7b94ae472499689182e8b
Softheitron <softheitron@gmail.com>
2024-10-14 14:16:37 +0500

    KS-13-4 telephone-book

---
+++ src/main/kotlin/lesson_13/lesson_13_task_4.kt
@@ -0,0 +1,47 @@
+package org.example.lesson_13
+
+class Contact4(val name: String, val number: Long, val company: String? = null) {
+    fun showContactInformation() {
+        println(
+            "Имя: $name\n" +
+                    "Номер: ${number}\n" +
+                    "Компания: ${company ?: "не указано"}"
+        )
+    }
+}
+
+fun main() {
+
+    val contactList = mutableListOf<Contact4>()
+
+    var isInProcess = true
+
+    do {
+        print("Введите имя контакта: ")
+        val nameInput = readln()
+        print("Введите номер контакта: ")
+        var numberInput = readln().toLongOrNull()
+        while (numberInput == null) {
+            print("Введен неверный формат номера!\nПопробуйте еще раз: ")
+            numberInput = readln().toLongOrNull()
+        }
+        print("Введите название компании: ")
+        var companyInput = readlnOrNull()
+        if (companyInput?.trim() == "") companyInput = null
+        contactList.add(
+            Contact4(
+            name = nameInput,
+            number = numberInput,
+            company = companyInput
+        )
+        )
+        print("Желаете добавить еще контакт?\nВведите \'Да\' или нажмите \'Enter\' что бы выйти: ")
+        val processFateInput = readln()
+        if (processFateInput.lowercase() != "да") isInProcess = false
+    } while (isInProcess)
+
+    contactList.forEach {
+        it.showContactInformation()
+    }
+
+}
