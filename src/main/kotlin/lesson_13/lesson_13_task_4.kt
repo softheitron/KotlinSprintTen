6570720f05c4125fdcde23b7c8ed5242eca0c325
Softheitron <softheitron@gmail.com>
2024-10-15 14:28:59 +0500

    KS-13-4 minor-refactor

--- src/main/kotlin/lesson_13/lesson_13_task_4.kt
+++ src/main/kotlin/lesson_13/lesson_13_task_4.kt
@@ -1,6 +1,6 @@
 package org.example.lesson_13
 
-class Contact4(val name: String, val number: Long?, val company: String? = null) {
+class Contact4(val name: String, val number: Long? = null, val company: String? = null) {
     fun showContactInformation() {
         println(
             "Имя: $name\n" +
@@ -21,17 +21,26 @@ fun main() {
         val nameInput = readln()
         print("Введите номер контакта: ")
         val numberInput = readln().toLongOrNull()
-        if (numberInput == null) println("Вы не ввели номер, графа номера останется пустой")
         print("Введите название компании: ")
         var companyInput = readlnOrNull()
         if (companyInput?.trim() == "") companyInput = null
-        contactList.add(
-            Contact4(
-            name = nameInput,
-            number = numberInput,
-            company = companyInput
-        )
-        )
+        if (numberInput == null) {
+            println("Вы не ввели номер телефона, графа номера останется пустой")
+            contactList.add(
+                Contact4(
+                    name = nameInput,
+                    company = companyInput
+                )
+            )
+        } else {
+            contactList.add(
+                Contact4(
+                    name = nameInput,
+                    number = numberInput,
+                    company = companyInput
+                )
+            )
+        }
         print("Желаете добавить еще контакт?\nВведите \'Да\' или нажмите \'Enter\' что бы выйти: ")
         val processFateInput = readln()
         if (processFateInput.lowercase() != "да") isInProcess = false
