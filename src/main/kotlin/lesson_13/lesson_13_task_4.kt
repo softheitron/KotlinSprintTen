d2425de8a4f70935a7089707b5acb2b767136af4
Softheitron <softheitron@gmail.com>
2024-10-16 15:00:01 +0500

    KS-13-4 refactor

--- src/main/kotlin/lesson_13/lesson_13_task_4.kt
+++ src/main/kotlin/lesson_13/lesson_13_task_4.kt
@@ -1,10 +1,10 @@
 package org.example.lesson_13
 
-class Contact4(val name: String, val number: Long? = null, val company: String? = null) {
+class Contact4(val name: String, val number: Long, val company: String? = null) {
     fun showContactInformation() {
         println(
             "Имя: $name\n" +
-                    "Номер: ${number ?: ""}\n" +
+                    "Номер: ${number}\n" +
                     "Компания: ${company ?: "не указано"}"
         )
     }
@@ -21,33 +21,44 @@ fun main() {
         val nameInput = readln()
         print("Введите номер контакта: ")
         val numberInput = readln().toLongOrNull()
+        if (numberInput == null) {
+            print(
+                "Вы не ввели номер телефона, такой контакт не будет добавлен\n" +
+                        "Начать заново или закрыть программу?\nДа/Нет? : "
+            )
+            val processFateInput = readln()
+            if (processFateInput.lowercase().trim() == "да") {
+                continue
+            } else {
+                println("До свидания!\n(все что вы успели занести в книгу отобразится ниже)")
+                isInProcess = false
+                continue
+            }
+        }
         print("Введите название компании: ")
         var companyInput = readlnOrNull()
         if (companyInput?.trim() == "") companyInput = null
-        if (numberInput == null) {
-            println("Вы не ввели номер телефона, графа номера останется пустой")
-            contactList.add(
-                Contact4(
-                    name = nameInput,
-                    company = companyInput
-                )
+
+        contactList.add(
+            Contact4(
+                name = nameInput,
+                number = numberInput,
+                company = companyInput
             )
-        } else {
-            contactList.add(
-                Contact4(
-                    name = nameInput,
-                    number = numberInput,
-                    company = companyInput
-                )
-            )
-        }
+        )
+
         print("Желаете добавить еще контакт?\nВведите \'Да\' или нажмите \'Enter\' что бы выйти: ")
         val processFateInput = readln()
-        if (processFateInput.lowercase() != "да") isInProcess = false
+        if (processFateInput.lowercase().trim() != "да") isInProcess = false
     } while (isInProcess)
 
+    println(
+        "Занесенные в книгу контакты:\n" +
+                "---------------------"
+    )
     contactList.forEach {
         it.showContactInformation()
+        println("---------------------")
     }
 
 }
