0d0075ecd18b3839b90e5a2293d72a86bad1e65c
Softheitron <softheitron@gmail.com>
2024-11-16 13:11:19 +0500

    KS-19-1 minor-changes

--- src/main/kotlin/lesson_19/lesson_19_task_1.kt
+++ src/main/kotlin/lesson_19/lesson_19_task_1.kt
@@ -1,16 +1,16 @@
 package org.example.lesson_19
 
-enum class Fishes {
-    GUPPY,
-    ANGELFISH,
-    GOLDFISH,
-    SIAMESE_FIGHTING_FISH,
+enum class Fishes(val cyrillic: String) {
+    GUPPY("Гуппи"),
+    ANGELFISH("Рыба-Ангел"),
+    GOLDFISH("Золотая Рыбка"),
+    SIAMESE_FIGHTING_FISH("Петушок"),
 }
 
 fun main() {
 
     Fishes.entries.forEach {
-        println(it.name)
+        println("${it.name} (${it.cyrillic})")
     }
 
 }
