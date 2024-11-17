9009ac0badf3a90d9451a21bebbb8b21f7ad1ed9
Softheitron <softheitron@gmail.com>
2024-11-17 14:27:29 +0500

    KS-18-1 minor-refactor

--- src/main/kotlin/lesson_18/lesson_18_task_1.kt
+++ src/main/kotlin/lesson_18/lesson_18_task_1.kt
@@ -1,43 +1,33 @@
 package org.example.lesson_18
 
-open class OrderItem {
+class OrderItem(val orderNumber: String) {
 
-    open fun getOrderInformation() = ""
-
-}
-
-class SingleItem(
-    private val orderNumber: String,
-    private val product: String,
-): OrderItem() {
-    override fun getOrderInformation(): String {
-        return "Заказан товар: $product"
+    fun getOrderInformation(product: String) {
+        println("Номер заказа: $orderNumber\n" +
+                "Наименование товара: $product")
     }
+
+    fun getOrderInformation(products: List<String>) {
+        println("Номер заказа: $orderNumber\n" +
+                "Наименование товарoв: ${products.joinToString()}")
+    }
+
 }
 
-class ListItem(
-    private val orderNumber: String,
-    private val product: List<String>,
-): OrderItem() {
-    override fun getOrderInformation(): String {
-        return "Заказаны следующие товары: ${product.joinToString() }"
-    }
-}
+
 
 fun main() {
 
-    val order1 = SingleItem(
-        "13213111",
-        "some product"
+    val order1 = OrderItem(
+        "13213111"
     )
 
-    val order2 = ListItem(
-        "1556657",
-        listOf("prod1", "prod2", "prod3")
+    val order2 = OrderItem(
+        "1556657"
     )
 
-    println(order1.getOrderInformation())
+    println(order1.getOrderInformation("SingleProduct"))
     println()
-    println(order2.getOrderInformation())
+    println(order2.getOrderInformation(listOf("multiple", "products", "test")))
 
 }
